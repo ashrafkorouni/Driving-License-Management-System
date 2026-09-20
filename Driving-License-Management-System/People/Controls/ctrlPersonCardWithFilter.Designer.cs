d539e4d393b93728c2b8429b3a6512e591ca4611
AshrafKorouni <ashrafkorouni86@gmail.com>
2026-09-20 13:11:07 +0300

    Add initial implementaion of ctrlPersonCardWithFilter control

---
+++ Driving-License-Management-System/People/Controls/ctrlPersonCardWithFilter.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace Driving_License_Management_System.People.Controls
+{
+    partial class ctrlPersonCardWithFilter
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.ctrlPersonCard1 = new Driving_License_Management_System.People.Controls.ctrlPersonCard();
+            this.gbFilters = new System.Windows.Forms.GroupBox();
+            this.btnAddNewPerson = new System.Windows.Forms.Button();
+            this.btnFind = new System.Windows.Forms.Button();
+            this.txtFilterValue = new System.Windows.Forms.TextBox();
+            this.cbFilterBy = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.gbFilters.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ctrlPersonCard1
+            // 
+            this.ctrlPersonCard1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ctrlPersonCard1.Location = new System.Drawing.Point(13, 102);
+            this.ctrlPersonCard1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.ctrlPersonCard1.Name = "ctrlPersonCard1";
+            this.ctrlPersonCard1.Size = new System.Drawing.Size(824, 297);
+            this.ctrlPersonCard1.TabIndex = 0;
+            // 
+            // gbFilters
+            // 
+            this.gbFilters.Controls.Add(this.btnAddNewPerson);
+            this.gbFilters.Controls.Add(this.btnFind);
+            this.gbFilters.Controls.Add(this.txtFilterValue);
+            this.gbFilters.Controls.Add(this.cbFilterBy);
+            this.gbFilters.Controls.Add(this.label1);
+            this.gbFilters.Location = new System.Drawing.Point(13, 17);
+            this.gbFilters.Name = "gbFilters";
+            this.gbFilters.Size = new System.Drawing.Size(824, 77);
+            this.gbFilters.TabIndex = 1;
+            this.gbFilters.TabStop = false;
+            this.gbFilters.Text = "Filter";
+            // 
+            // btnAddNewPerson
+            // 
+            this.btnAddNewPerson.Image = global::Driving_License_Management_System.Properties.Resources.AddPerson_32;
+            this.btnAddNewPerson.Location = new System.Drawing.Point(677, 23);
+            this.btnAddNewPerson.Name = "btnAddNewPerson";
+            this.btnAddNewPerson.Size = new System.Drawing.Size(63, 37);
+            this.btnAddNewPerson.TabIndex = 20;
+            this.btnAddNewPerson.UseVisualStyleBackColor = true;
+            this.btnAddNewPerson.Click += new System.EventHandler(this.btnAddNewPerson_Click);
+            // 
+            // btnFind
+            // 
+            this.btnFind.Image = global::Driving_License_Management_System.Properties.Resources.SearchPerson;
+            this.btnFind.Location = new System.Drawing.Point(594, 23);
+            this.btnFind.Name = "btnFind";
+            this.btnFind.Size = new System.Drawing.Size(63, 37);
+            this.btnFind.TabIndex = 19;
+            this.btnFind.UseVisualStyleBackColor = true;
+            this.btnFind.Click += new System.EventHandler(this.btnFind_Click);
+            // 
+            // txtFilterValue
+            // 
+            this.txtFilterValue.Location = new System.Drawing.Point(358, 28);
+            this.txtFilterValue.Multiline = true;
+            this.txtFilterValue.Name = "txtFilterValue";
+            this.txtFilterValue.Size = new System.Drawing.Size(198, 28);
+            this.txtFilterValue.TabIndex = 2;
+            this.txtFilterValue.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtFilterValue_KeyPress);
+            this.txtFilterValue.Validating += new System.ComponentModel.CancelEventHandler(this.txtFilterValue_Validating);
+            // 
+            // cbFilterBy
+            // 
+            this.cbFilterBy.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbFilterBy.FormattingEnabled = true;
+            this.cbFilterBy.Items.AddRange(new object[] {
+            "National No",
+            "Person ID"});
+            this.cbFilterBy.Location = new System.Drawing.Point(138, 28);
+            this.cbFilterBy.Name = "cbFilterBy";
+            this.cbFilterBy.Size = new System.Drawing.Size(198, 28);
+            this.cbFilterBy.TabIndex = 1;
+            this.cbFilterBy.SelectedIndexChanged += new System.EventHandler(this.cbFilterBy_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(52, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Filter By:";
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // ctrlPersonCardWithFilter
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.Controls.Add(this.gbFilters);
+            this.Controls.Add(this.ctrlPersonCard1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "ctrlPersonCardWithFilter";
+            this.Size = new System.Drawing.Size(849, 399);
+            this.Load += new System.EventHandler(this.ctrlPersonCardWithFilter_Load);
+            this.gbFilters.ResumeLayout(false);
+            this.gbFilters.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private ctrlPersonCard ctrlPersonCard1;
+        private System.Windows.Forms.GroupBox gbFilters;
+        private System.Windows.Forms.Button btnAddNewPerson;
+        private System.Windows.Forms.Button btnFind;
+        private System.Windows.Forms.TextBox txtFilterValue;
+        private System.Windows.Forms.ComboBox cbFilterBy;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+    }
+}
---
+++ Driving-License-Management-System/People/Controls/ctrlPersonCardWithFilter.cs
@@ -0,0 +1,172 @@
+﻿using DVLD_Business;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Driving_License_Management_System.People.Controls
+{
+    public partial class ctrlPersonCardWithFilter : UserControl
+    {
+        // Define a custom event handler delegate with parameters
+        public event Action<int> OnPersonSelected;
+        // Create a protected method to raise the event with a parameter
+        protected virtual void PersonSelected(int PersonID)
+        {
+            Action<int> handler = OnPersonSelected;
+            if (handler != null)
+            {
+                handler(PersonID);  // Raise the event with the parameter
+            }
+        }
+
+        private bool _ShowAddPerson = true;
+
+        public bool ShowAddPerson
+        {
+            get { return _ShowAddPerson; }
+
+            set
+            {
+                _ShowAddPerson = value;
+                btnAddNewPerson.Visible = _ShowAddPerson;
+            }
+        }
+
+        private bool _FilterEnabled = true;
+
+        public bool FilterEnabled
+        {
+            get { return _FilterEnabled; }
+
+            set
+            {
+                _FilterEnabled = value;
+                gbFilters.Enabled = _FilterEnabled;
+            }
+        }
+
+        private int _PersonID;
+
+        public int PersonID
+        {
+            get { return ctrlPersonCard1.PersonID; }
+        }
+
+        public void LoadPersonInfo(int PersonID)
+        {
+            cbFilterBy.SelectedIndex = 1;
+            txtFilterValue.Text = PersonID.ToString();
+            _FindNow();
+        }
+
+        private void _FindNow()
+        {
+            switch(cbFilterBy.Text)
+            {
+                case "Person ID":
+                    ctrlPersonCard1.LoadPersonInfo(int.Parse(txtFilterValue.Text));
+
+                    break;
+
+                case "National No":
+                    ctrlPersonCard1.LoadPersonInfo(txtFilterValue.Text);
+
+                    break;
+
+                default:
+                    break;
+            }
+
+            if(OnPersonSelected != null && FilterEnabled)
+            {
+                OnPersonSelected(ctrlPersonCard1.PersonID);
+            }
+        }
+
+        public clsPerson SelectedPersonInfo
+        {
+            get { return ctrlPersonCard1.SelectedPersonInfo; }
+        }
+
+        public ctrlPersonCardWithFilter()
+        {
+            InitializeComponent();
+        }
+
+        private void cbFilterBy_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            txtFilterValue.Text = "";
+            txtFilterValue.Focus();
+        }
+
+        private void btnFind_Click(object sender, EventArgs e)
+        {
+            if(!this.ValidateChildren())
+            {
+                MessageBox.Show("Some fileds are not valide!, put the mouse over the red icon(s) to see the erro", "Validation Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+
+            }
+            _FindNow();
+        }
+
+        private void ctrlPersonCardWithFilter_Load(object sender, EventArgs e)
+        {
+            cbFilterBy.SelectedIndex = 0;
+            txtFilterValue.Focus ();
+        }
+
+        private void txtFilterValue_Validating(object sender, CancelEventArgs e)
+        {
+            if(string.IsNullOrEmpty(txtFilterValue.Text.Trim()))
+            {
+                e.Cancel = true;
+                errorProvider1.SetError(txtFilterValue, "This Field Is Required!");
+            }
+            else
+            {
+                e.Cancel= false;
+                errorProvider1.SetError(txtFilterValue, null);
+            }
+        }
+
+        private void btnAddNewPerson_Click(object sender, EventArgs e)
+        {
+            frmAddUpdatePerson frm = new frmAddUpdatePerson();
+            frm.DataBack += DataBackEvent;
+            frm.ShowDialog();
+        }
+
+        private void DataBackEvent(object sender, int PersonID)
+        {
+            cbFilterBy.SelectedIndex = 1;
+            txtFilterValue.Text = PersonID.ToString();
+            ctrlPersonCard1.LoadPersonInfo(PersonID);
+        }
+
+        public void FilterFocus()
+        {
+            txtFilterValue.Focus();
+        }
+
+        private void txtFilterValue_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            // Chech if the pressed key is Enter (chatacter code 13)
+            if(e.KeyChar == (char)13)
+            {
+                btnFind.PerformClick();
+            }
+
+            if(cbFilterBy.Text == "Person ID")
+            {
+                e.Handled = !char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar);
+            }
+        }
+    }
+}
